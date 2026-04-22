a6efc984bb4023989764bff9ed1e46b57f8dc8b0
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-22 10:14:08 -0500

    feat(layout): integrate news sources and articles components with state management.

--- src/shared/presentation/components/layout.vue
+++ src/shared/presentation/components/layout.vue
@@ -1,13 +1,34 @@
 <script setup>
-import {ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import LanguageSwitcher from "./language-switcher.vue";
 import FooterContent from "./footer-content.vue";
+import {newsStore} from "../../../news/application/news.store.js";
+import SourceList from "../../../news/presentation/components/source-list.vue";
+import ArticleList from "../../../news/presentation/components/article-list.vue";
+import UnavailableContent from "../../../news/presentation/components/unavailable-content.vue";
 
 const drawerVisible = ref(false);
 
 const toggleDrawer = () => {
   drawerVisible.value = !drawerVisible.value;
 }
+
+const sources = computed(() => newsStore.sources);
+const errors = computed(() => newsStore.errors);
+let articles = computed(() => newsStore.articles);
+const rerenderKey = ref(0);
+
+const setSource = source => {
+  newsStore.setCurrentSource(source);
+  articles = computed(() => newsStore.articles);
+  rerenderKey.value += 1; // Force re-render of ArticleList
+  toggleDrawer();
+};
+
+onMounted(() => {
+  newsStore.loadSources();
+  rerenderKey.value += 1; // Force re-render of ArticleList
+});
 
 </script>
 
@@ -17,7 +38,9 @@ const toggleDrawer = () => {
     <pv-menubar>
       <template #start>
         <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer" />
-        <!-- TODO: Add source list -->
+        <source-list v-model:sources="sources"
+                     v-model:visible="drawerVisible"
+                     v-on:source-selected="setSource($event)"/>
       </template>
       <template #end>
         <language-switcher></language-switcher>
@@ -26,7 +49,8 @@ const toggleDrawer = () => {
   </div>
 </div>
 <div>
-  <!-- TODO: Add article list -->
+  <article-list v-if="articles" v-model:articles="articles" :key="rerenderKey"/>
+  <unavailable-content v-else :errors="errors"></unavailable-content>
 </div>
 <footer-content></footer-content>
 </template>
